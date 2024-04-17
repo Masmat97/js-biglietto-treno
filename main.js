0fd52c7b5246e6bdf2c1cbc8eed8baf2bd49e34e
Matteo Mascio <matteo.mascio@libero.it>
2024-04-17 16:45:01 +0200

    2

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ console.log(`Anni: ${age}`)
 let km = prompt("Inserisci il numero di km da percorrere")
 console.log(`km: ${km}`)
 
-var prezzo = prompt("0.21€")
 
-let prezzoBiglietto = ("km * 0.21")
-console.log("")
+let prezzo = 0.21€
+console.log("€: ${€} ")
