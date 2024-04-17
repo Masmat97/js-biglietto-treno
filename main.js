a7528e8f81bc1729b312a63d8e62d746c8da51a7
Matteo Mascio <matteo.mascio@libero.it>
2024-04-17 15:59:20 +0200

    ok

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let age = prompt("Inserisci la tua età");
+const age = prompt("Inserisci la tua età");
 console.log(`Anni: ${age}`)
-let km = prompt("Inserisci il numero di km da percorrere")
+const km = prompt("Inserisci il numero di km da percorrere")
 console.log(`km: ${km}`)
