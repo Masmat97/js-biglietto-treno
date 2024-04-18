81b544b553dec22a1b80a0f68192d8cc1fe0a5e3
Matteo Mascio <matteo.mascio@libero.it>
2024-04-18 15:48:54 +0200

    first

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-let age = prompt("Inserisci la tua età");
+const age = prompt("Inserisci la tua età");
 console.log(`Anni: ${age}`)
 
-let km = prompt("Inserisci il numero di km da percorrere")
+const km = prompt("Inserisci il numero di km da percorrere")
 console.log(`km: ${km}`)
 
+const prezzo = 0.21
+console.log(`prezzo: ${prezzo}`)
 
-let prezzo = 0.21€
-console.log("€: ${€} ")
+
+
+const prezzoBiglietto = prezzo * km
+console.log(`prezzoBiglietto: ${prezzoBiglietto}`)
+
+
+if (age < 18)  {
+    console.log(`Applico lo sconto del 20%`);
+   
+
+    const finalPrice = prezzoBiglietto * 0.8;
+    console.log(`Prezzo sconato del 20%:`, finalPrice);
+}
+
+
+else if (age > 65)  {
+    console.log(`Applico lo sconto del 40%`);
+   
+
+    const finalPrice = prezzoBiglietto * 0.6;
+    console.log(`Prezzo sconato del 40%:`, finalPrice);
+}
